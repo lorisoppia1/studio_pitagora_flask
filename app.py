80bd794b4ba1d69148166100c32abcccadc47c16
Loris <loris.oppia@devinterface.com>
2025-09-02 12:56:47 +0200

    Fix port on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,6 @@ def echo():
     return jsonify({"received": data})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 

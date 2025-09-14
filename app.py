cd3edf8146ed13c1db00989806a4aa70f4f23360
Loris <loris.oppia@devinterface.com>
2025-09-14 17:11:02 +0200

    Fix on chi_siamo page

--- app.py
+++ app.py
@@ -74,9 +74,4 @@ if __name__ == '__main__':
         db.create_all()
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-
-# COSE DA FARE
-# 2 - adatta a smartphone !!!!!
-# 4 - fai ultime schermate della home page !!!!!
+    app.run(host="0.0.0.0", port=port, debug=True)

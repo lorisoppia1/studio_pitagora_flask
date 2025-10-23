5be97592c5670919ba95ae10cf5ce3e13d5cb34c
Loris <loris.oppia@devinterface.com>
2025-10-23 10:22:03 +0200

    Responsive pages chi_siamo, contatti, navbar and footer

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         db.create_all()
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 # sistemare bottone chiama
 # sistemare da telefono i nostri servizi, e la navbar
